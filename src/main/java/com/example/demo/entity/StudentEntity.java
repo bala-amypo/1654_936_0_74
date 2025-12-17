7a105beed782fc5f6ddcfacf72f26c1fb4c6d406
bala-amypo <bala@amypo.in>
2025-12-17 08:11:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,72 +1,66 @@
-
 package com.example.demo.entity;
 
-
-import java.sql.Date;
-import jakarta.persistance.Entity;
-import jakarta.persistance.GeneratedValue;
-import jakarta.persistance.GenerationType;
-import jakarta.persistance.Id;
-
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 @Entity
 public class StudentEntity{
     @Id
-    @GeneratedValue(stragey=GenerativeType.IDENTITY)
-    private String name;
-    private Integer id;
-    private String email;
-    private Date created;
-    private String password;
-    public String getCreated()
-    {
-        return created;
-    }
-    public void setCreated(Date created)
-    {
-        this.created=created;
-    }
-    public Integer getId()
-    {
-        return id;
-    }
-    public void setId(Integer id)
-    {
-        this.id=id;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public void setName(String name)
-    {
-        this.name=name;
-    }
-    public String getEmail()
-    {
-        return email;
-    }
-    public void setEmail(String email)
-    {
-        this.email=email;
-    }
-    public String getPassword()
-    {
-        return password;
-    }
-    public void setPassword(String password)
-    {
-        this.password=password;
-    }
-    public StudentEntity(Integer id,String name,String email,String password,Date created)
-    {
-        this.id=id;
-        this.name=name;
-        this.email=email;
-        this.password=password;
-        this.created=created;
-    }
-    public StudentEntity()
-    {
-        
-    }
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+   private Integer id;
+   private String name;  
+   private String email;
+   private String password;
+   private Date created;
+  //ID
+   public Integer getId(){
+    return id;
+   }
+   public void setId(Integer id){
+    this.id=id;
+   }
+   //Name
+   public String getName(){
+    return name;
+   }
+   public void setName(String name){
+    this.name=name;
+   }
+ 
+   //Email
+   public String getEmail(){
+    return email;
+   }
+   public void setEmail(String email){
+    this.email=email;
+   }
+   //Password
+   public String getPassword(){
+    return password;
+   }
+   public void setPassword(String password){
+    this.password=password;
+   }
+   //Created
+     //ID
+   public Date getCreated(){
+    return created;
+   }
+   public void setCreated(Date created){
+    this.created=created;
+   }
+    
+//Parameter constructor
+public StudentEntity(Integer id,String name,String email,String Password){
+    this.id=id;
+    this.name=name;
+    this.email=email;
+    this.password=password;
+    this.created=created;
+}
+//Default Constructor
+public StudentEntity(){
+}
 }
