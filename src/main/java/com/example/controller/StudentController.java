92376c1363d44fd7689dcdda8cb4f6541c13056e
bala-amypo <bala@amypo.in>
2025-12-18 09:36:09 +0000

    User commit

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,38 +1,38 @@
 package com.example.demo.controller;
-
+import java.util.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.service.StudentService;
-import java.util.List;
-
 @RestController
 public class StudentController{
     @Autowired StudentService ser;
     @PostMapping("/post")
-    public StudentEntity sendData(@RequestBody StudentEntity entity){
-            return ser.postdata(entity);
+    public StudentEntity sendData(@RequestBody StudentEntity stu){
+        return ser.postData(stu);
     }
-    @GetMapping("/Get")
-    public List<StudentEntity>getval(){
-        return ser.getAllData();
+    @GetMapping("/get")
+    public List<StudentEntity> geAllData(){
+        return ser.getData();
     }
     @DeleteMapping("/delete/{id}")
-    public String deleteval(@PathVariable int id){
-        return ser.DeleteData(id);
+    public String deleteVal(@PathVariable int id){
+        return ser.deleteData(id);
     }
-    @GetMapping("/getid/{id}")
-    public StudentEntity putval(@PathVariable int id){
-        return service.getData(id);
-    }
+    @GetMapping("/getData/{id}")
+    public StudentEntity FindVal(@PathVariable int id){
+        return ser.findData(id);
+    }  
     @PutMapping("/put/{id}")
-    public StudentEntity putval(@PathVariable int id,@RequestBody StudentEntity entity){
-    return service.updateData(id,entity);
-}
+    public StudentEntity UpdateData(@PathVariable int id,@RequestBody StudentEntity stu){
+            return ser.updateData(id,stu);
+    }
 
 }
---
+++ src/main/java/com/example/controller/ValidationController.java
@@ -0,0 +1,19 @@
+
+package com.example.demo.controller;
+import java.util.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.entity.ValidationEntity;
+import com.example.demo.service.ValidationService;
+import jakarta.validation.Valid;
+@RestController
+public class ValidationController{
+    @Autowired ValidationService serve;
+    @PostMapping("/data")
+    public ValidationEntity sendData(@Valid @RequestBody ValidationEntity stu){
+        return serve.post(stu);
+    }
+}
