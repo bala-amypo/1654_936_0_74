6437865e41d0d55a2af9f84c5007e64bb86a1f2c
bala-amypo <bala@amypo.in>
2025-12-18 08:06:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,16 +1,23 @@
 package com.example.demo.entity
 
-omport jakarta.persistance.Entity;
-import jakart.persistanceGenerated
-import jakart.persistance
-import jakart.persistance
-import jakart.persistance
+import jakarta.persistance.Entity;
+import jakart.persistance.GeneratedValue;
+import jakart.persistance.GenerationType;
+import jakart.persistanceId;
+import jakart.persistance.constrains.NotNull;
+import jakart.persistance.constrains.Size;
 
 @Entity
 public class ValidiationEntity{
-    public Long id;
+@Id
+@GeneratedValue(stragy = GenerationType.IDENTY)
+private Long id;
+@Not Null
+@Size(min = 2, max = , message = "must br 2 to 10 character")
     private String username;
+    @Email(message = "Email is not valid")
     private String email;
+    
     private String password;
     private int age;
 
