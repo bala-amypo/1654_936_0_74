a3116b4911d85a29359d74f7218dba95a947b5b1
bala-amypo <bala@amypo.in>
2025-12-18 05:21:09 +0000

    User commit

--- src/main/java/com/example/controller/StudentController.java
+++ src/main/java/com/example/controller/StudentController.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import com.example.demo.entity.StudentEntity;
+import com.example.demo.entity.Studententity;
 import com.example.demo.service.StudentService;
 import java.util.List;
+
 @RestController
 public class StudentController{
     @Autowired StudentService ser;
     @PostMapping("/post")
-    public StudentEntity sendData(@RequestBody StudentEntity entity){
+    public Studententity sendData(@RequestBody StudentEntity entity){
             return ser.postdata(entity);
     }
     @GetMapping("/Get")
@@ -19,7 +23,9 @@ public class StudentController{
         return ser.getAllData();
     }
     @DeleteMapping("/delete/{id}")
-    public String deleteval(@PathVariable){
-        return service.
+    public String deleteval(@PathVariable int id){
+        return ser.DeleteData(id);
     }
 }
+
+
