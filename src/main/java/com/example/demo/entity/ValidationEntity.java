a9a37ac3c63f51604ee753bba864b8ad97262114
bala-amypo <bala@amypo.in>
2025-12-18 07:56:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity
+
+@Entity
+public class ValidiationEntity{
+    public Long id;
+    private String username;
+    private String email;
+    private String password;
+    private int age
+}
